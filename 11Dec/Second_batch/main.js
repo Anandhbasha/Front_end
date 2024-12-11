879815cfe693339d33100d5b3ba356d345938d3d
Anandhbasha <anadhakm@gmail.com>
2024-12-11 12:52:46 +0530

    last commit

---
+++ 11Dec/Second_batch/main.js
@@ -0,0 +1,16 @@
+//Hoisting
+    //var - 
+
+var a = 10;
+console.log("the a value is" + a);
+
+    //let - 
+    let b = 10;
+    b=20;
+    a=b;
+    console.log("the addition value is" + (a + b));
+    
+    //const
+    const x = 20;
+    console.log("Const Value is" + x);
+    
