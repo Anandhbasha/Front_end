ff98d14b156ab43c39e443c343964c9a57d1a29c
Anandhbasha <anadhakm@gmail.com>
2024-12-16 12:56:23 +0530

    Second Commit

---
+++ 16Dec/Second_Batch/main.js
@@ -0,0 +1,82 @@
+//Hoisting
+// var
+// var x;
+// x=10;
+// x=20;
+// console.log(x);
+
+// let
+// let y = 20;
+// y=41;
+// y= y+1;
+// y++;
+// y--;
+
+// console.log(y);
+
+// const
+// const z =30;
+
+// operators
+// =, +, - ,*,/,%,++,--,==,===
+
+
+// let a =y/z;
+// console.log(a);
+
+// let b = y%2;
+// console.log(b);
+
+// let name = "1"
+// let name1  = 1
+
+// if(name===name1){
+//     console.log("Both are same names");
+    
+// }
+// else{
+//     console.log("Both are not Same");
+    
+// }
+// Datatypes
+//Primitive
+// numbers-integer
+// let names = 10;
+// let p = names;
+// console.log(p);
+
+//string
+// let name = "Name";
+// console.log(name);
+//undifined
+// Boolean
+// let a;
+// let p = 10;
+// let x = true;
+// console.log(typeof(x));
+// console.log(typeof(p));
+// console.log(typeof(a));
+
+//NonPrimitive_datatypes
+//Array
+// const Cars = ["RR","Bently","Audi","Jaquar","BMW"];
+
+// console.log(Cars[1]);
+
+//object
+    // let person = {
+    //     name:"Anandh",
+    //     age:30,
+    //     Gender:"Male",
+    //     isavlive:true,
+    //     Qualification:{
+    //         degree:"B.E",
+    //         Job:"Developer"
+    //     }
+    // }
+    //  console.log(person.Qualification.Job);
+    
+
+//function
+
+
