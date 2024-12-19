669ea49561754fd42a6e8f1020ad54e7208e1d5f
Anandhbasha <anadhakm@gmail.com>
2024-12-19 09:03:31 +0530

    first commit

---
+++ 18Dec/Second_batch/main.js
@@ -0,0 +1,18 @@
+
+function add (){
+    let x = 10;
+    x+=20;
+    alert(x);
+    console.log(x);    
+}
+
+//loop for.while do while
+
+let y = 150;
+let x = 120;
+
+while (y > x) {
+
+    console.log("The value is Grater then"); 
+    break  
+}
