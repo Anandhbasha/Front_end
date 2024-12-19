3c5047def71fbc7743de0bd089ca30633415838b
Anandhbasha <anadhakm@gmail.com>
2024-12-19 13:23:11 +0530

    last commit

---
+++ 19Dec/Second_batch/main.js
@@ -0,0 +1,68 @@
+
+// // function add (){
+// //     let x = 10;
+// //     x+=20;
+// //     alert(x);
+// //     console.log(x);    
+// // }
+
+// // //loop for.while do while
+
+// // let y = 150;
+// // let x = 120;
+
+// // while (y > x) {
+
+// //     console.log("The value is Grater then"); 
+// //     break  
+// // // }
+// // function add(a){
+// //     // let x =20;
+// //     // x+=x;
+// //     a+=a;
+// //     alert(a);
+// //     // x = x+20
+// //     console.log(a);
+    
+// // }
+
+// // let cars = ["bmw","tata","audi","kia"]
+// // console.log(cars[0]);
+// // console.log(cars[3]);
+
+// // const dharani={
+// //     name:"dharani",
+// //     color:"white",
+// //     height:5, 
+// //     age:25,
+// //     occupation:{
+// //         degree:"agri",
+// //         job:"developer",
+// //         status:{
+// //             commited :true,
+// //         }
+// //     }
+
+// // }
+
+// // console.log(typeof(dharani))
+
+// // arr=[1,2,3,3,4,4,55,6]
+// // for(let a=0;a<=arr.length;a++){
+    
+// //     console.log(arr[a] + "im always right");
+
+// // }
+
+// // let x=60;
+// // let y=60;
+// // while(x==y){
+// // console.log("values are equal");
+// // break;
+
+// // }
+
+// do{
+// console.log("im useless");
+
+// }while(5!=10)
