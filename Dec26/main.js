590d09ef32b58fa32e7c5da525b2543cd360ac6b
Anandhbasha <anadhakm@gmail.com>
2024-12-27 10:45:03 +0530

    first commit

--- Dec26/main.js
+++ Dec26/main.js
@@ -1,36 +1,36 @@
 //var
-var x=10;
+// var x=10;
 
-x =50;
+// x =50;
 
-console.log(x);
+// console.log(x);
 
 //let
-let y;
+// let y;
 // y = 20;
 // y = 100;
 // console.log(y);
 
 //const  
-const a = "";
+// const a = "";
 
 // a = "name1";
 // console.log(a);
 
 //operators
 // =,+,-,*,/,%,++,--
-let c = 3;
+// let c = 3;
 // console.log(c%b);
 
 // console.log(b);
 // c--;
 // console.log(c);
-let b = 2;
+// let b = 2;
 //b=b+;
-b=b++;
-console.log(b);
-let d = (b+b)*c;
-console.log(d);
+// b=b++;
+// console.log(b);
+// let d = (b+b)*c;
+// console.log(d);
 
 
 //20
@@ -44,7 +44,7 @@ console.log(d);
 
 
 
-b++;
+// b++;
 // console.log(++b);
 // console.log(b);
 
@@ -55,5 +55,122 @@ b++;
 
 
 
-
         
+// var X = 50;
+// // var x =100;
+
+// console.log(x);
+
+// let a = 50;
+// a = 60;
+
+// const b = 10;
+// b=20;
+
+
+// arithmetic operators
+
+// let number1 = 5;
+// let nums = null;
+// let un;
+// let number2 = 10;
+// let isAvlive = true;
+
+// let num = "abc"
+// console.log( number1+number2);
+// console.log( number1 - number2);
+// console.log("Multiple" + number1*number2);
+// console.log("Divide" + number1/number2);
+// console.log("Modulus" + number1%number2);
+// console.log("square root" + number1**number2);
+// console.log(num/2);
+
+
+
+// let name = "xyz"
+// let age = 35
+// let address= "cbe";
+// let zipcode = 636000;
+// let inSideTheCity = true;
+
+
+
+
+// //object
+// const person = {
+//     name:"xyz",
+//     age:35,
+//     address:"coimbatore",
+//     zipcode:636600,
+//     inSideTheCity:true,
+//     qualification:{
+//         sslc:40,
+//         hsc:30
+//     }
+// }
+
+// person.siblings = "brother";
+// delete person.address;
+// console.log(person);
+
+
+// //array 
+// const color = ["red", "green", "Yellow"]
+
+// color[4]="pink";
+// color[3]="purple";
+// console.log(color);
+
+const students = [{
+    studentName:"Arun",
+    rollNumber:1254421,
+    marks:[50,70,80,30,100]
+},
+{
+    studentName:"bala",
+    rollNumber:1254422,
+    marks:[50,70,80,30,100]
+},
+{
+    studentName:"pranav",
+    rollNumber:1254421,
+    marks:[50,70,80,30,100]
+},
+{
+    studentName:"mithun",
+    rollNumber:1254421,
+    marks:[50,70,80,30,100]
+},
+{
+    studentName:"anandh",
+    rollNumber:1254421,
+    marks:[50,70,80,30,100]
+},
+{
+    studentName:"raja",
+    rollNumber:1254421,
+    marks:[50,70,80,30,100]
+},
+{
+    studentName:"mano",
+    rollNumber:1254421,
+    marks:[50,70,80,30,100]
+}]
+
+// console.log(students.length);
+
+
+
+
+
+//loop
+
+// for(let x=6;x>=0;x--){
+//     console.log(students[x]);
+// }
+
+function add(a,b){
+    alert(a+b);
+    
+}
+
