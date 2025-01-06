9ca68a2558b15bfa6dda27b47e16d8da13e0e718
Anandhbasha <anadhakm@gmail.com>
2025-01-06 10:17:59 +0530

    first batch

---
+++ Jan6/script.js
@@ -0,0 +1,109 @@
+//Hoisting
+//For
+//If
+//Functions
+//Operatores
+//Data Types
+//MRF
+// let arr = [10,20,30,40,55]
+// varibalename.map((localvariable)=>{
+    
+// })
+// arr.map((x)=>console.log(x));
+
+//Filter
+// let newOne = arr.filter((y)=>y==20)
+// console.log(newOne);
+
+//reduce
+// let fill = arr.reduce((add,accumalator)=>add+accumalator,0);
+// console.log(fill);
+
+//ternary operator
+
+// let age = 10;
+
+// let category = age >=18 ? "Adult":"Teen";
+// console.log(category);
+
+//JSON
+
+
+// let age = 18;
+// if(age < 18){
+//     console.log("Teenager");
+    
+// }else if(age >=18){
+//     console.log("Mens");
+    
+// }
+// else{
+//     console.log("He is Adult");
+    
+// }
+
+//Switch
+
+// let day = "Tuesday";
+
+// switch (day) {
+//     case "Sunday":
+//         console.log("Today is sunday");
+        
+//         break;
+//     case "Monday":
+//     console.log("Today is Monday");
+//     break
+        
+
+//     default:
+//         console.log("Today is Tuesday");
+        
+//         break;
+// }
+
+//for of
+
+// let arr = [10,20,30,40,55]
+
+// let [ara,ara1,ara2] = arr;
+// console.log(ara,ara1,ara2);
+ 
+
+// let person = {name:"Xyz",age:20}
+
+// for(let [x,value] of Object.entries(person)){
+//     if(value=="Xyz"){
+//         console.log(x , value);
+//     }
+    
+    
+// }
+
+//destructuring
+// let {name,age} = person;
+// console.log(name,age);
+
+
+// for(let x of arr){
+//     console.log(x);
+    
+// }
+// for(let x in arr){
+//     console.log(x);
+//     console.log(arr[x]);
+// }
+
+//while
+// let i = 10;
+// while(i<15){
+//     console.log("Hello");
+//     i++;    
+// }
+//do
+// do {
+//     console.log("Welcome");
+//     i++;
+    
+// } while (i==0);
+
