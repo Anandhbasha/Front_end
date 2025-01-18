5ca865fd21ae41f94deecb2bcc94e58041cb55e0
Anandhbasha <anadhakm@gmail.com>
2025-01-18 11:39:54 +0530

    Operators

---
+++ Jan18/main.js
@@ -0,0 +1,87 @@
+//Hoisting
+//Type,variable name
+
+//var
+//function scope
+    // var x =10;
+    
+    // var x=30;
+    // console.log(x);
+    
+//let
+//local scope variable
+    // let y;
+    // y=x;
+
+    // console.log(y);
+    
+//const
+// local scope variable
+// const b =60;
+// console.log(b);
+
+//operators
+//assignment operator
+//=
+// let a = 30;
+
+//arithmetic operator
+//+,-,/,*,%,++,--
+// let z = 5;
+// let s = z%2;
+// console.log(s);
+
+let b = 20;
+
+let c = b;
+console.log(++b);
+
+
+console.log(c);
+
+
+// types of data primitive data types
+//number
+//string
+//boolean
+//undefined
+//NULL
+//NaN
+
+
+//Non Premitive data types
+//array
+//object
+//functions
+
+
+//conditional operator
+// ==,===comparison operator
+//Grater then
+let d = 10;
+let h = 50;
+console.log(d<=h);
+
+// less then 
+//AND
+// let x  = 20;
+// let y = "20"
+
+// let a = 20;
+// let d= 20;
+
+// console.log(x==y);
+
+
+// console.log(x==y && a==d);
+
+
+//OR
+// console.log(x==y || a==d);
+// NOT
+
+// console.log(x!=y);
+
+
+
+//ternary operator
