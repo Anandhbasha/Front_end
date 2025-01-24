d947981b63597828c4bbe8cb24de298ab07afd0f
Anandhbasha <anadhakm@gmail.com>
2025-01-24 11:02:58 +0530

    data types

---
+++ Jan24/main.js
@@ -0,0 +1,81 @@
+//operator
+    //comaprison opeartors
+        // <,>,>=,<=,!=,
+
+// let a = 20;
+// let b = "20";
+
+
+// console.log(a!=b);
+// console.log(a<=b);
+// console.log(a>=b);
+// console.log(a<b);
+// console.log(a>b);
+// console.log(a==b);
+// console.log(a===b);
+
+
+// Data Types
+//premetive data types
+
+// let a = 10;
+// let b = "xyz";
+// let c = true;
+// let z;
+// let d = null
+
+// // console.log(a);
+
+// //non premitive data types
+// // array
+// let arr = [10,50,60,70,50]
+
+// console.log(arr[1]);
+
+// //object
+// const person = {
+//     name:"xyz",
+//     age:24,
+//     DOB:"",
+//     family:{
+//         daddy:"abc",
+//         mom:"dfr",
+//         siblings:{
+//             sister:"jhg",
+//             brother:"jkkh"
+//         }
+//     }
+// }
+
+// console.log(person.family.siblings.sister);
+
+
+//function
+// let x = 20;
+// let g =30;
+// console.log(x+g);
+// let f = 20;
+// let r = 40;
+// console.log(f+r);
+
+// function add(num1,num2){
+//     if(num1<num2){
+//         console.log(num1+num2);
+        
+//     }
+    
+// }
+// add(10,20)
+// add(50,20)
+// add(60,20)
+// add(30,20)
+// add(10,40)
+// add(10,70)
+// add(100,20)
+// add(10,200)
+// add(300,20)
+// add(10,500)
+
+
+
+
