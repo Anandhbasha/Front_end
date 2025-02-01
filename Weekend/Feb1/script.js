2cbdbb65aa3785ddaeb2f1f7b17f554a9f86f57f
Anandhbasha <anadhakm@gmail.com>
2025-02-01 14:03:09 +0530

    js

---
+++ Weekend/Feb1/script.js
@@ -0,0 +1,22 @@
+// let h1 = document.createElement('h1');
+// h1.textContent = "Hello"
+// h1.id = "head"
+// h1.setAttribute("class","head");
+// h1.style.backgroundColor="Red"
+
+// let arr = ["sno","name","age"]
+
+// let table = document.createElement("table")
+// let thead= document.createElement("thead")
+// arr.map((th)=>{
+//      th = document.createElement("th")
+// })
+
+// let tbody= document.createElement("tbody")
+
+// table.appendChild(thead)
+// table.appendChild(tbody)
+// document.body.append(table);
+
+// let person = document.querySelector('h1');
+// person.textContent = "I am Very Bad Guy" 
---
+++ Weekend/Feb1/scripts.js
@@ -0,0 +1,60 @@
+//while
+let a = 10;
+let b= 20;
+
+let x = [10,50,60,80,88]
+
+// while(d<x.length){
+//     console.log(x[d]);
+//     d++;    
+// }
+
+//do while
+// do{
+//     console.log("Do while");
+    
+// }while(a>b)
+//for
+// for(let d=0;d<x.length;d++){
+//     console.log(x[d])
+// }
+// const person = {
+//     name:"xyz",
+//     age:30,
+//     gender:"male"
+// }
+
+// Object.entries(person).map((x)=>{console.log(x)})
+
+// ES6
+// function
+function name (){
+}
+name()
+//arrow
+const arrow = ()=>{}
+arrow()
+//IFFI
+const iffi = (function(){})
+iffi()
+//Ananoums
+const ana = function(){}
+
+//for of
+// for(let d=0;d<x.length;d++){
+//         console.log(x[d])
+//     }
+// for(let d of x){
+//     console.log(d);    
+// }
+//for in
+// for(let z in x){
+//     console.log(x[z]);
+    
+// }
+//foreach
+// x.forEach((y)=>console.log(y))
+//Map
+x.map((u)=>console.log(u))
+//reduce
+//filter
