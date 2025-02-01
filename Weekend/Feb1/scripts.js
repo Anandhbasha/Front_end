0ba2db1ee27a56a7cb8e07282108abe546b55f5f
Anandhbasha <anadhakm@gmail.com>
2025-02-01 14:46:10 +0530

    settimeout

--- Weekend/Feb1/scripts.js
+++ Weekend/Feb1/scripts.js
@@ -2,7 +2,7 @@
 let a = 10;
 let b= 20;
 
-let x = [10,50,60,80,88]
+
 
 // while(d<x.length){
 //     console.log(x[d]);
@@ -18,13 +18,8 @@ let x = [10,50,60,80,88]
 // for(let d=0;d<x.length;d++){
 //     console.log(x[d])
 // }
-// const person = {
-//     name:"xyz",
-//     age:30,
-//     gender:"male"
-// }
 
-// Object.entries(person).map((x)=>{console.log(x)})
+// Object.keys(person).map((x)=>{console.log(x)})
 
 // ES6
 // function
@@ -55,6 +50,94 @@ const ana = function(){}
 //foreach
 // x.forEach((y)=>console.log(y))
 //Map
-x.map((u)=>console.log(u))
+
+// x.map((u)=>console.log(u))
 //reduce
-//filter
+// let reduce = x.reduce((acc,curr)=>acc+curr)
+// console.log(reduce);
+
+// //filter
+// let fill = x.filter((f)=>f%2===0)
+// // console.log(fill);
+
+// // x.pop()
+// // console.log(x);
+// x.push(100)
+
+// x.shift()
+// x.unshift(500)
+// // console.log(x);
+// let find = x.find((c)=>c==400)
+// console.log(find);
+// console.log(x.indexOf(80));
+// console.log(x.includes(506));
+// let j = 10.40
+// console.log(Math.round(j));
+// console.log(Math.random());
+
+//slice
+
+let g =0
+// x.map((item)=>{
+//     if(g<item){
+//         g=item
+//         //g=10
+//         //g=50
+//         //g=80
+//         //85
+//     }
+
+// })
+// console.log(g);
+
+// console.log(x.slice(1,2));
+//split 
+// let names = "Kadhar/Basha"
+// let new_name =names.split("/")
+// console.log(new_name);
+
+
+let x = [10,85,,20,80,50]
+let x1 = [100,805,250,8850,5670]
+// x[2]=500
+// x[0]=15
+// let l =x;
+// console.log(x);
+
+// //rest operator
+// let[first,second,...rest] = x1
+// console.log("First value is " + first);
+// console.log("Second value is " + second);
+// console.log("rest value is " + rest);
+
+// spread operator
+
+let arr = [...x,...x1]
+// console.log(arr);
+
+//destructure
+
+// let[first1,second2] = x1
+// console.log(first1);
+// console.log(second2);
+// const person = {
+//     Username:"xyz",
+//     age:30,
+//     gender:"male"
+// }
+
+// const {Username,gender}= person
+// console.log(`userName is: ${Username}`);
+
+//setTimeout
+// setTimeout(()=>{
+//     console.log("Hello");
+    
+// },5000)
+
+// setInterval(()=>{
+//     console.log("Welcome");
+    
+// },5000)
+
+
