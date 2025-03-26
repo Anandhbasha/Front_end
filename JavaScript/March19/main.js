dc972ce5d4295fd76fb24b37bd0242a84b9756ef
Anandhbasha <anadhakm@gmail.com>
2025-03-26 12:46:36 +0530

    last commit

--- JavaScript/March19/main.js
+++ JavaScript/March19/main.js
@@ -1,73 +1,240 @@
-// //Hoisting
-// //var
-//     console.log(a);
-//     var a = 10;
-//     a = 30;
-//     var a = 50;    
-//     console.log(a);
-// //let
+// // // //Hoisting
+// // // //var
+// // //     console.log(a);
+// // //     var a = 10;
+// // //     a = 30;
+// // //     var a = 50;    
+// // //     console.log(a);
+// // // //let
 
-// // let b = 50;
-// // b=70;
-// // let b = 20
-// let b;
-// console.log(b);
-// //const
-// const c =60;
-// c=80;
-// console.log(c);
+// // // // let b = 50;
+// // // // b=70;
+// // // // let b = 20
+// // // let b;
+// // // console.log(b);
+// // // //const
+// // // const c =60;
+// // // c=80;
+// // // console.log(c);
 
-//function scope 
-//local scope
+// // //function scope 
+// // //local scope
 
-// function add(){
+// // // function add(){
     
-//     if(10<15){
-//         var b = 60     
-//     }
-//     console.log(b);
-// }
-// add()
+// // //     if(10<15){
+// // //         var b = 60     
+// // //     }
+// // //     console.log(b);
+// // // }
+// // // add()
 
-// //operators
-//     //arithmetic operator
-//         // +,-,*,/,%,++,--,=
-//         let a = 20;
-//         let b = 10;
-//         console.log(a+b);
-//         console.log(a-b);
-//         console.log(a*b);
-//         console.log(a/b);
-//         console.log(a%b);
-//         console.log(++a);
-        // console.log(a);
-    //logical opetrator
-        // &&,||,!
-        // if(10>20 && 5<6 && 50<100){
-        //     console.log("And is working");            
-        // }
-        // if(10>20 || 5<6){
-        //     console.log("Or is working");            
-        // }
-        // if(10!=10){
-        //     console.log("Not Equal");
+// // // //operators
+// // //     //arithmetic operator
+// // //         // +,-,*,/,%,++,--,=
+// // //         let a = 20;
+// // //         let b = 10;
+// // //         console.log(a+b);
+// // //         console.log(a-b);
+// // //         console.log(a*b);
+// // //         console.log(a/b);
+// // //         console.log(a%b);
+// // //         console.log(++a);
+// //         // console.log(a);
+// //     //logical opetrator
+// //         // &&,||,!
+// //         // if(10>20 && 5<6 && 50<100){
+// //         //     console.log("And is working");            
+// //         // }
+// //         // if(10>20 || 5<6){
+// //         //     console.log("Or is working");            
+// //         // }
+// //         // if(10!=10){
+// //         //     console.log("Not Equal");
             
-        // }
-// >=,<=,>,<,==,===
-if(10>=10){
-    console.log("Working");    
-}
+// //         // }
+// // // >=,<=,>,<,==,===
+// // if(10>=10){
+// //     console.log("Working");    
+// // }
 
-if(10=="20"){
-    console.log("Double");    
-}
-// if(10==="10"){
-//     console.log("Triple");    
-// }else{
-//     console.log("Not");
+// // if(10=="20"){
+// //     console.log("Double");    
+// // }
+// // // if(10==="10"){
+// // //     console.log("Triple");    
+// // // }else{
+// // //     console.log("Not");
     
+// // // }
+
+// // //Ternary operator
+// // let age = 10;
+// // console.log(age>=18?"Adult":"Teenager");
+
+// //permitive data types
+// var a = 10;
+// var name = "xyz"
+// var isAlive = true
+// var b;
+// var x = null;
+// //Nan---Not a number
+// console.log(typeof(x));
+
+// console.log(typeof(b));
+
+// console.log(typeof(a));
+// console.log(typeof(name));
+// console.log(typeof(isAlive));
+
+
+// //non premitive data types
+
+// //array
+// const arr = [10,"gjgjg",true,""]
+// //array-index
+// console.log(arr);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(typeof(arr));
+
+// //object
+// const person = {
+//     userName:"xyz",
+//     userAge:30,
+//     userLocation:"Cbe",
+//     isAlive:true
+// }
+// console.log(person);
+// console.log(person.userName);
+// // console.log(person.userAge);
+
+
+// //functions
+// function add(num1,num2){
+//     console.log(num1+num2);    
+// }
+// add(10,20)
+// add(100,200)
+// let a =10
+// let b=20
+// console.log(a+b);
+// let d = 100;
+// let c = 200;
+// console.log(c+d);
+
+//IFFI
+// const iifi = (function (){
+
+// })
+// //Arrow
+// const arrow = ()=>console.log("Hello")
+
+// arrow();
+
+// //anonumous
+// const anonu = (){
+
 // }
 
-//Ternary operator
-let age = 10;
-console.log(age>=18?"Adult":"Teenager");
+//statements
+    //if
+    // let x = 10;
+    // let g = 30;
+    // // if(g>x){
+    // //     console.log("G is Grater");
+        
+    // // }
+    // //if else
+    // if(g<x){
+    //     console.log("X is Grater");        
+    // }
+    // else{
+    //     console.log("G is grater");        
+    // }
+    //elseif
+    let age = 19;
+    if(age<9){
+        console.log("He is Child");
+        
+    }
+    else if(age<=18 && age>=9){
+        console.log("Teenager");
+        
+    }
+    else{
+        console.log("Adult");        
+    }
+
+    //switch
+    let day = "wednesday"
+
+    switch(day){
+        case "monday":
+            console.log("Today is Monday");
+            break
+        case "tuesday":
+            console.log("Today is tuesday");
+            break
+        case "wednesday":
+            console.log("Today is wednesday");
+            break
+        case "Thursday":
+            console.log("Today is Thursday");
+            break
+        case "Friday":
+            console.log("Today is Friday");
+            break
+        case "Saturday":
+            console.log("Today is Saturday");
+            break
+        default:
+            console.log("Today is Sunday");
+            
+    }
+
+
+    //loops
+    //while
+    let a = 50;
+    let b= 55;
+        // while(a<b){
+        //     a++
+        //     console.log("While is working");            
+        // }
+
+    //do while
+    // do{
+    //     console.log("Do while is working");
+    //     a++
+    // }while(a>b)
+
+    // for
+    let arr = [50,70,60,80,90]
+    // for(let x=10;x<a;x++){
+    //     console.log("a value is"+a);        
+    // }
+
+    // for(let x=0;x<arr.length;x++){
+    //     console.log("Array value is" + arr[x]);
+        
+    // }
+
+    //for of
+    // for(let x of arr){
+    //     console.log(x);
+        
+    // }
+    // //for in
+    // for(let x in arr){
+    //     console.log(arr[x]);
+        
+    // }
+    //foreach
+    arr.forEach((x)=>console.log(x))
+
+
+
+
